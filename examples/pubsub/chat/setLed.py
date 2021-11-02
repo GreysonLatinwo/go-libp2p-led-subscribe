0f8cd96f415e89f678c867d964828f1242ccdcbb
Greyson Latinwo <greysonlatinwo@gmail.com>
2021-11-02 17:56:21 -0500

    led static presets

--- examples/pubsub/chat/setLed.py
+++ examples/pubsub/chat/setLed.py
@@ -1,5 +1,8 @@
+import re
+import os
 import board
 import neopixel
+import adafruit_fancyled.adafruit_fancyled as fancy
 import threading
 import socket
 
@@ -11,9 +14,12 @@ bindsocket.listen(5)
 print("listening...")
 
 def clientInputLoop(sock, fromaddr):
-    NUM_LEDS = 512
+    NUM_LEDS = int(os.environ['NUMLEDS'])
     BRIGHTNESS = 0.1
+    t1 = None
+    stopPreset = False
     pixels = neopixel.NeoPixel(board.D18, NUM_LEDS, brightness=BRIGHTNESS, auto_write=False)
+
     def check_decimal(s) -> bool:
         try:
             float(s)
@@ -22,40 +28,107 @@ def clientInputLoop(sock, fromaddr):
         else:
             return True
     
-    def is_clean(clientData) -> bool:
-        if clientData == '':
-            return False
-        if clientData.count(',') < 2:
-            return False
-        dataSplit = clientData.split(',')
-        if not check_decimal(dataSplit[0]) or not check_decimal(dataSplit[1]) or not check_decimal(dataSplit[2]):
-            return False
-        if len(dataSplit) == 4 and not check_decimal(dataSplit[3]):
-            return False
-        return True
+    def preset1(stop):
+        # Declare a 6-element RGB rainbow palette
+        palette = [
+            fancy.CRGB(0.66, 0.0, 1.0),  # purple
+            fancy.CRGB(0.0, 1.0, 1.0),  # nitro blue
+            fancy.CRGB(0.0, 0.75, 0.5),  # mint green
+        ]
+
+        offset = 0  # Positional offset into color palette to get it to 'spin'
+
+        while not stop():
+            for i in range(NUM_LEDS):
+                # Load each pixel's color from the palette using an offset, run it
+                # through the gamma function, pack RGB value and assign to pixel.
+                color = fancy.palette_lookup(palette, offset + i / NUM_LEDS)
+                color = fancy.gamma_adjust(color, brightness=1.0)
+                pixels[i] = color.pack()
+            pixels.show()
+
+            offset += 0.002  # Bigger number = faster spin
+        print('Preset1 Exited')
+
+    def preset2(stop):
+        # Declare a 6-element RGB rainbow palette
+        palette = [
+            fancy.CRGB(1.0, 0.0, 0.0), # red
+            fancy.CRGB(1.0, 0.0, 1.0),  # purple
+            fancy.CRGB(1.0, 0.0, 0.5), # pink
+        ]
+
+        offset = 0  # Positional offset into color palette to get it to 'spin'
+
+        while not stop():
+            for i in range(NUM_LEDS):
+                # Load each pixel's color from the palette using an offset, run it
+                # through the gamma function, pack RGB value and assign to pixel.
+                color = fancy.palette_lookup(palette, offset + i / NUM_LEDS)
+                color = fancy.gamma_adjust(color, brightness=1.0)
+                pixels[i] = color.pack()
+            pixels.show()
+
+            offset += 0.002  # Bigger number = faster spin
+        print('Preset2 Exited')
 
     while True:
         try:
             #read led data from the socket
             clientData = sock.recv(24).decode('utf-8').strip() #expected format: 'R,G,B[,Brightness]'
-            if not is_clean(clientData):
+            
+            if clientData == '':
+                break
+
+            #check if the input is clean
+            is_clean = True
+            match = re.fullmatch("preset\d", clientData)
+            #if the client is setting the preset otherwise check rgb color
+            if match and clientData in dir():
+                is_clean = True 
+            else:
+                if clientData.count(',') < 2:
+                    is_clean = False
+                dataSplit = clientData.split(',')
+                if not check_decimal(dataSplit[0]) or not check_decimal(dataSplit[1]) or not check_decimal(dataSplit[2]):
+                    is_clean = False
+                if len(dataSplit) == 4 and not check_decimal(dataSplit[3]):
+                    is_clean = False
+            if not is_clean:
                 print('ignoring:', clientData)
                 continue
-            dataSplit = clientData.split(',')
-            #if the brightness was sent, reset the pixel brightness
-            if len(dataSplit) == 4:
-                print(pixels.brightness)
-                pixels.brightness = float(dataSplit[3])
-            #set all led color
-            print(fromaddr, '->',(int(dataSplit[0]), int(dataSplit[1]), int(dataSplit[2])))
-            pixels.fill((int(dataSplit[0]), int(dataSplit[1]), int(dataSplit[2])))
-            # #update pixels
-            pixels.show()
-        except:
+            
+            if match:
+                if t1 and t1.is_alive:
+                    stopPreset = True
+                    t1.join()
+                stopPreset = False
+                if match.string == "preset1":
+                    t1 = threading.Thread(target=preset1, args=((lambda: stopPreset),))
+                    t1.start()
+                elif match.string == "preset2":
+                    t1 = threading.Thread(target=preset2, args=((lambda: stopPreset),))
+                    t1.start()
+            else:
+                dataSplit = clientData.split(',')
+                #if the brightness was sent, reset the pixel brightness
+                if len(dataSplit) == 4:
+                    pixels.brightness = float(dataSplit[3])
+                    print("Brighness="+str(pixels.brightness))
+                #set all led color
+                
+                pixels.fill((int(dataSplit[0]), int(dataSplit[1]), int(dataSplit[2])))
+                #update pixels
+                pixels.show()
+            print(fromaddr, '->', clientData)
+        except Exception as e:
             sock.shutdown(2)    # 0 = done receiving, 1 = done sending, 2 = both1
             sock.close()
             # connection error event here, maybe reconnect
-            print('connection error')
+            print('Error:', e, type(e))
+            return
+        except ConnectionResetError as eConn:
+            print('ConnectionResetError:', eConn)
             return
 
 while True:
